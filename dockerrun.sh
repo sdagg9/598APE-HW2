31648b81697863304c4bcd54bcdd762152fb4937
npunati27 <npunati2@illinois.edu>
2025-10-15 15:41:42 -0500

    script to run benchmarks

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,2 +1,1 @@
-docker run -it --security-opt seccomp=unconfined -v `pwd`:/host wsmoses/598ape /bin/bash
-
+docker run -it -v "$(pwd)":/host --privileged --security-opt seccomp=unconfined wsmoses/598ape /bin/bash
